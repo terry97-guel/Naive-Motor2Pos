75cdaae8d9f04c116eacda613dea936b975a630a
terry97-guel <taerimyoon@Korea.ac.kr>
2022-02-04 10:30:24 +0900

    change loss function for Multi log spiral

--- loss.py
+++ loss.py
@@ -2,12 +2,9 @@
 import torch
 
 
-class Get_Loss_Function():
-    def Pos_norm2(self,output, label):
-        loss = torch.nn.MSELoss()(output,label)
-        return loss
-    
-    
-    # def se3_norm2(output, label):
-        #Fill me
+
+def Pos_norm2(output, label):
+    loss = torch.nn.MSELoss()(output,label)
+    return loss
+
 
