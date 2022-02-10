1ddd930e8f60e72c286987915b5efd64df51ec20
terry97-guel <taerimyoon@Korea.ac.kr>
2022-02-10 17:35:39 +0900

    chainge dataloader for SOROsim Grid data

--- dataloader.py
+++ dataloader.py
@@ -8,7 +8,8 @@ import os
 
 
 class ToyDataset(Dataset):
-    def __init__(self,data_path):
+    def __init__(self,data_path,IOScale=None):
+
         if os.path.isdir(data_path):
             Scenario_ls = os.listdir(data_path)
             self.label = torch.tensor([])
@@ -16,27 +17,51 @@ class ToyDataset(Dataset):
 
             for scenario in Scenario_ls:
                 file_path = data_path + '/' + scenario
-                rawdata = np.loadtxt(file_path)
-                self.label = torch.cat((self.label,torch.Tensor(rawdata[:,:3])),0)
-                self.input = torch.cat((self.input,torch.Tensor(rawdata[:,3:])),0)
+                rawdata = np.loadtxt(file_path, delimiter=',')
+                self.label = torch.cat((self.label,torch.Tensor(rawdata[:,3:])),0)
+                self.input = torch.cat((self.input,torch.Tensor(rawdata[:,:3])),0)
                 
-        if os.path.isfile(data_path):
+        elif os.path.isfile(data_path):
             file_path = data_path
             rawdata = np.loadtxt(file_path)
-            self.label = torch.Tensor(rawdata[:,:3])
-            self.input = torch.Tensor(rawdata[:,3:])
+            self.label = torch.Tensor(rawdata[:,3:])
+            self.input = torch.Tensor(rawdata[:,:3])
+        
+
+        if IOScale != None:
+            
+            (inputSigma,inputMean),(outputSigma,outputMean) = IOScale
+            inputSigma,inputMean,outputSigma,outputMean = inputSigma.detach(),inputMean.detach(),outputSigma.detach(),outputMean.detach()
+            
+            #repeat
+            outputMean = outputMean.tile(self.label.size()[-1]//3)
+            outputSigma  = outputSigma.tile(self.label.size()[-1]//3)
+            
+            self.input = 10*(self.input - inputMean)/inputSigma
+            self.label = 10*(self.label - outputMean)/outputSigma
+
 
     def __len__(self):
         return len(self.input)
     
     def __getitem__(self,idx):
         return self.input[idx], self.label[idx]
+    
+    def getIOScale(self):
+        (inputVar,inputMean),(outputVar,outputMean) = (torch.var_mean(self.input,0), torch.var_mean(self.label[:,-3:],0))
+        
+        inputVar[inputVar<1e-9]   =  1
+        outputVar[outputVar<1e-9] =  1
+
+        inputSigma, outputSigma = torch.sqrt(inputVar), torch.sqrt(outputVar)
+
+        
+        return ((inputSigma,inputMean),(outputSigma,outputMean))
 
 class ToyDataloader(DataLoader):
-    def __init__(self,data_path, n_workers,batch, shuffle=True):
-        self.dataset = ToyDataset(data_path)
+    def __init__(self,data_path, IOScale, n_workers,batch, shuffle = True):
+        self.dataset = ToyDataset(data_path, IOScale)
         super().__init__(self.dataset, batch_size=batch, shuffle=shuffle, num_workers=n_workers)
-  
 
 class FoldToyDataset(Dataset):
     def __init__(self,data_path,Foldstart,Foldend):
